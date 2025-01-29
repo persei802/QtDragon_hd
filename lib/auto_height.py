31bb6c138a903f0d74dff7798cc2eee28fffe417
Jim Sloot <persei802@gmail.com>
2025-01-29 10:26:48 -0700

    rearranged ui and fixed standalone mode

--- lib/auto_height.py
+++ lib/auto_height.py
@@ -18,13 +18,11 @@ import json
 
 from PyQt5 import QtGui, QtWidgets, uic
 from qtvcp.core import Info, Status, Action, Path
-from qtvcp import logger
 
 INFO = Info()
 STATUS = Status()
 ACTION = Action()
 PATH = Path()
-LOG = logger.getLogger(__name__)
 HERE = os.path.dirname(os.path.abspath(__file__))
 HELP = os.path.join(PATH.CONFIGPATH, "help_files")
 
@@ -32,13 +30,16 @@ WARNING = 1
 
 
 class Auto_Measure(QtWidgets.QWidget):
-    def __init__(self, widget, handler, parent=None):
+    def __init__(self, widget=None, handler=None, parent=None):
         super(Auto_Measure, self).__init__()
         self.w = widget
         self.h = handler
         self.parent = parent
         self.helpfile = 'height_measure_help.html'
         self.stat = linuxcnc.stat()
+        self.send_dict = {}
+        self.red_border = "border: 2px solid red;"
+        self.line_list = ['pos_x1', 'pos_y1', 'pos_z1', 'pos_x2', 'pos_y2', 'pos_z2']
         if INFO.MACHINE_IS_METRIC:
             self.valid = QtGui.QDoubleValidator(-999.999, 999.999, 3)
         else:
@@ -50,19 +51,12 @@ class Auto_Measure(QtWidgets.QWidget):
         except AttributeError as e:
             print("Error: ", e)
 
-        self.normal_color = ""
-        self.error_color = "color: #ff0000;"
-        self.send_dict = {}
         # define validators for all lineEdit widgets
-        for i in ['x1', 'y1', 'z1', 'x2', 'y2', 'z2']:
-            self['lineEdit_pos_' + i].setValidator(self.valid)
-
-        # signal connections
-        self.chk_enable_set.stateChanged.connect(self.chk_enable_changed)
-        self.btn_set_wp.clicked.connect(self.set_position_clicked)
-        self.btn_set_machine.clicked.connect(self.set_position_clicked)
-        self.btn_start.clicked.connect(self.start)
-        self.btn_help.pressed.connect(self.show_help)
+        for i in self.line_list:
+            self['lineEdit_' + i].setValidator(self.valid)
+        # set units according to machine type
+        unit = 'MM' if INFO.MACHINE_IS_METRIC else 'IN'
+        self.lbl_height_unit.setText(unit)
 
     def _hal_init(self):
         self.actionbutton_abort.hal_init()
@@ -72,6 +66,15 @@ class Auto_Measure(QtWidgets.QWidget):
         STATUS.connect('state_estop', lambda w: self.setEnabled(False))
         STATUS.connect('interp-idle', lambda w: self.setEnabled(homed_on_status()))
         STATUS.connect('all-homed', lambda w: self.setEnabled(True))
+
+        # signal connections
+        self.chk_enable_set.stateChanged.connect(self.chk_enable_changed)
+        self.btn_set_wp.clicked.connect(self.set_position_clicked)
+        self.btn_set_machine.clicked.connect(self.set_position_clicked)
+        self.btn_start.clicked.connect(self.start)
+        self.btn_help.pressed.connect(self.show_help)
+
+        self.default_style = self.w.lineEdit_search_vel.styleSheet()
         
     def chk_enable_changed(self, state):
         self.btn_set_wp.setEnabled(state)
@@ -105,6 +108,7 @@ class Auto_Measure(QtWidgets.QWidget):
         self.send_dict['max_probe'] = self.w.lineEdit_max_probe.text()
         self.send_dict['retract_distance'] = self.w.lineEdit_retract.text()
         self.send_dict['z_safe_travel'] = self.w.lineEdit_zsafe.text()
+
         self.send_dict['pos_x1'] = self.lineEdit_pos_x1.text()
         self.send_dict['pos_y1'] = self.lineEdit_pos_y1.text()
         self.send_dict['pos_z1'] = self.lineEdit_pos_z1.text()
@@ -112,62 +116,56 @@ class Auto_Measure(QtWidgets.QWidget):
         self.send_dict['pos_y2'] = self.lineEdit_pos_y2.text()
         self.send_dict['pos_z2'] = self.lineEdit_pos_z2.text()
         string_to_send = 'probe_z$' + json.dumps(self.send_dict) + '\n'
-        print("String to send ", string_to_send)
+#        print("String to send ", string_to_send)
         rtn = ACTION.AUTO_HEIGHT(string_to_send, self.autoheight_return, self.autoheight_error)
         if rtn == 0:
             self.h.add_status("Autoheight routine is already running", WARNING)
 
     def validate(self):
         valid = True
-        # create lineEdit colors to indicate error state
-        # placed here because changing stylesheets could change colors
-        default_color = self.w.lineEdit_probe_tool.palette().color(self.w.lineEdit_probe_tool.foregroundRole())
-        self.normal_color = f"color: {default_color.name()};"
-        # restore normal text colors
-        for item in ["search_vel", "probe_vel", "max_probe", "retract", "zsafe"]:
-            widget = self.w['lineEdit_' + item]
-            color = widget.palette().color(widget.foregroundRole())
-            if color.name() == "#ff0000":
-                widget.setStyleSheet(self.normal_color)
+        # restore normal border colors
+        for item in ["zsafe", "search_vel", "probe_vel", "max_probe"]:
+            if self.w['lineEdit_' + item].styleSheet() == self.red_border:
+                self.w['lineEdit_' + item].setStyleSheet(self.default_style)
         # check search velocity
         try:
             if float(self.w.lineEdit_search_vel.text()) <= 0.0:
-                self.w.lineEdit_search_vel.setStyleSheet(self.error_color)
+                self.w.lineEdit_search_vel.setStyleSheet(self.red_border)
                 self.h.add_status("Search velocity must be greater than 0.0", WARNING)
                 valid = False
         except:
-            self.w.lineEdit_search_vel.setStyleSheet(self.error_color)
+            self.w.lineEdit_search_vel.setStyleSheet(self.red_border)
             valid = False
         # check probe velocity
         try:
             if float(self.w.lineEdit_probe_vel.text()) <= 0.0:
-                self.w.lineEdit_probe_vel.setStyleSheet(self.error_color)
+                self.w.lineEdit_probe_vel.setStyleSheet(self.red_border)
                 self.h.add_status("Probe velocity must be greater than 0.0", WARNING)
                 valid = False
         except:
-            self.w.lineEdit_probe_vel.setStyleSheet(self.error_color)
+            self.w.lineEdit_probe_vel.setStyleSheet(self.red_border)
             valid = False
         # check max probe distance
         try:
             if float(self.w.lineEdit_max_probe.text()) <= 0.0:
-                self.w.lineEdit_max_probe.setStyleSheet(self.error_color)
+                self.w.lineEdit_max_probe.setStyleSheet(self.red_border)
                 self.h.add_status("Max probe must be greater than 0.0", WARNING)
                 valid = False
         except:
-            self.w.lineEdit_max_probe.setStyleSheet(self.error_color)
+            self.w.lineEdit_max_probe.setStyleSheet(self.red_border)
             valid = False
         # check retract distance
         try:
             if float(self.w.lineEdit_retract.text()) <= 0.0:
-                self.w.lineEdit_retract.setStyleSheet(self.error_color)
+                self.w.lineEdit_retract.setStyleSheet(self.red_border)
                 self.h.add_status("Retract distance must be greater than 0.0", WARNING)
                 valid = False
             elif float(self.w.lineEdit_retract.text()) > float(self.w.lineEdit_max_probe.text()):
-                self.w.lineEdit_retract.setStyleSheet(self.error_color)
+                self.w.lineEdit_retract.setStyleSheet(self.red_border)
                 self.h.add_status("Retract distance must be less than max_probe", WARNING)
                 valid = False
         except:
-            self.w.lineEdit_retract_distance.setStyleSheet(self.error_color)
+            self.w.lineEdit_retract.setStyleSheet(self.red_border)
             valid = False
         # check z safe height
         try:
@@ -179,11 +177,11 @@ class Auto_Measure(QtWidgets.QWidget):
                     error = "Z Safe height must be > than P1 Z height"
                 else:
                     error = "P1 Z height must be >= P2 Z height"
-                self.w.lineEdit_zsafe.setStyleSheet(self.error_color)
+                self.w.lineEdit_zsafe.setStyleSheet(self.red_border)
                 self.h.add_status(error, WARNING)
                 valid = False
         except:
-            self.w.lineEdit_zsafe.setStyleSheet(self.error_color)
+            self.w.lineEdit_zsafe.setStyleSheet(self.red_border)
             valid = False
         return valid
 
@@ -200,7 +198,7 @@ class Auto_Measure(QtWidgets.QWidget):
         self.h.add_status("Height measurement successfully completed")
 
     def autoheight_error(self, data):
-        self.h.add_status(data)
+        self.h.add_status(data, WARNING)
 
 # required code for subscriptable iteration
     def __getitem__(self, item):
